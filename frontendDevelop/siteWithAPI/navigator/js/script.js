0ff6dbb71da3f45d90ec34f42df8ec19aa68440d
hairhelgen <sultan.albert@yandex.ru>
2021-12-21 23:19:38 +0300

    modefied this folder

--- frontendDevelop/siteWithAPI/navigator/js/script.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-fetch("https://api.mapbox.com/v4/mapbox.mapbox-streets-v8/12/1171/1566.mvt?style=mapbox://styles/mapbox/streets-v11@00&access_token=pk.eyJ1IjoiYWxiZXJ0LXN1bHRhbm92IiwiYSI6ImNrd3Y0dW44bTAwOWszMHM4MWx4N2Jib2EifQ.NFop9tk6l6NPKZTJ5LGqVg")
-  .then((response) => response.json())
-  .then((body) => {
-    // console.log(body?.data);
-  })
